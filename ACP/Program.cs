7d5262e8f4ba73448ac4dd3ee6f85353d6a432c9
Jerry <jerry27syd@gmail.com>
2013-02-13 02:40:13 +1100

    test

--- ACP/Program.cs
+++ ACP/Program.cs
@@ -7,7 +7,7 @@ namespace ACP
     {
         private static void Main(string[] args)
         {
-            var list = new List<string>();
+            var lists = new List<List<string>>();
 
             var noun = new List<string>();
             noun.Add("Jerry");
@@ -23,30 +23,11 @@ namespace ACP
             subject.Add("a book");
             subject.Add("a cake");
             subject.Add("a cake");
-
-
-            var lists = new List<List<string>>();
-
             lists.Add(noun);
             lists.Add(verb);
-            lists.Add(subject);
+            lists.Add(verb);
 
-
-            var indexer = new Indexer(lists);
-
-
-            while (!indexer.End)
-            {
-                var str = string.Empty;
-                for (int i = 0; i < indexer.Indices.Count; i++)
-                {
-                    int index = indexer.Indices[i];
-                    
-                    str += lists[i][index] + " ";
-                }
-                list.Add(str);
-                indexer.Increment();
-            }
+            new ListGenerator(lists).Generate().ForEach(Console.WriteLine);
 
             Console.ReadKey();
         }
